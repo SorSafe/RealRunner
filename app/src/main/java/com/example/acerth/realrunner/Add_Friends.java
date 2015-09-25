b58885b7423b271884843c5499b8bd94da9d8c80
SorSafe <safedeeva@gmail.com>
2015-09-25 19:57:17 +0700

    add search view for pop

--- app/src/main/java/com/example/acerth/realrunner/Add_Friends.java
+++ app/src/main/java/com/example/acerth/realrunner/Add_Friends.java
@@ -1,12 +1,17 @@
 package com.example.acerth.realrunner;
 
 import android.app.Activity;
+import android.app.SearchManager;
+import android.app.SearchableInfo;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 
 /**
  * Created by Admin PC on 8/9/2558.
  */
 public class Add_Friends extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,5 +19,12 @@ public class Add_Friends extends Activity {
 
 //        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linear);
 //        linearLayout.setBackgroundColor(Color.parseColor("#00b06b"));
+    }
+
+    private void setupSearchView() {
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        final SearchView searchView = (SearchView) findViewById(R.id.searchView);
+        SearchableInfo searchableInfo = searchManager.getSearchableInfo(getComponentName());
+        searchView.setSearchableInfo(searchableInfo);
     }
 }
